45e72295391788f972bf8f29aec9f5b239b19314
CarmineC93 <107880239+CarmineC93@users.noreply.github.com>
2022-11-14 14:18:59 +0100

    first

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script>
+import axios from "axios";
+import { store } from "./store";
+
+import HelloWorld from './components/HelloWorld.vue'
+export default {
+  components: {
+
+    },
+    data(){
+      return{
+        store,
+
+      }
+    },
+  }
+</script>
+
+<template>
+
+</template>
+
+<style>
+
+</style>
